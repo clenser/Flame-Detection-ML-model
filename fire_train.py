0449a33b10741329df4b35ffda9448e4d9e6b169
Sreemanta Barman <100501976+clenser@users.noreply.github.com>
2025-03-02 20:32:51 +0530

    Update fire_train.py

--- fire_train.py
+++ fire_train.py
@@ -5,7 +5,7 @@ def train_model():
     epochs = 85  # Total number of training epochs
 
     # Train the model
-    model.train(data=r"C:\Users\srbar\Desktop\Fire\dataset\data.yaml",
+    model.train(data=r"C:\Users\srbar\Desktop\Fire\dataset\data.yaml", #path to data.yaml file
                 epochs=epochs, imgsz=640, batch=8, device="cuda", workers=0)
 
 if __name__ == "__main__":  # Prevent multiprocessing issues on Windows
